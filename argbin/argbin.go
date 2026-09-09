c940c2b4b684267326e14ddf29aa60741a0346b1
Tyy47 <Tylerwmack321@gmail.com>
2026-09-09 08:02:26 -0400

    wip(doc): fully documentated argbin.go

--- argbin/argbin.go
+++ argbin/argbin.go
@@ -13,88 +13,130 @@ var (
 	ErrUnknownCommand = errors.New("unknown command")
 )
 
-// Context is a list that can hold values for later use.
+// Context is a list of data that can be used to store and access data.
 type Context struct {
-	Command *Command
-	Values  map[string]any
-	Args []string
-	AdditionalArgs []string // Args that start from index 3 (length of 4)
-	ParsedValue string
+	// Command stores the executed command. Can be accessed for command fields.
+	Command *Command 
+
+	// Values stores needed context between flags and commands.
+	Values  map[string]any 
+
+	// Args gathered from os.Args, starts at os.Args[1:].
+	Args []string 
+
+	// AdditionalArgs that start from index 3 (length of 4).
+	AdditionalArgs []string 
+
+	// Value that is gathered after a command. (e.g "appname" "command" "parsedvalue")
+	ParsedValue string 
 }
 
+// Validate checks if a context object is valid for processing, returns an error if it's not.
 func (c *Context) Validate() error {
+	// Checks if context is nil, returns an error if so.
 	if c == nil {
 		return ErrNilContext
 	}
-
+	
+	// Checks if contexts command is nil, if so, it'll create a command and continue.
 	if c.Command == nil {
 		c.Command = &Command{}
 	}
-
+	
+	// Checks if context Values is nil, if so, it'll create a string:any map.
 	if c.Values == nil {
 		c.Values = make(map[string]any)
 	}
-
+	
+	// Return nil to satisfy return
 	return nil
 }
 
+// ToggleValue switches values inside of Context.Values, returns an error if unable to make changes.
 func (ctx *Context) ToggleValue(key string, toggle bool) error {
-	if _, ok := ctx.Values[key].(string); ok || !ok {
+
+	// Checks if the given key is found inside of ctx.Values
+	if _, ok := ctx.Values[key].(string); ok {
+
+		// If it's found, it'll toggle the value to whatever toggle is equal to.
 		ctx.Values[key] = toggle
 		return nil
+
 	} else {
+
+		// Returns an error if a key isn't found in the values map.
 		return fmt.Errorf("%s doesn't exist in context values", key)
 	}
 }
 
+// GetValue retrieves a value from a given key, returns the value and an error if failed.
 func (ctx *Context) GetValue(key string) (any, error) {
+
+	// Attempts to find a value based on the given key
 	if token, ok := ctx.Values[key]; ok {
+
+		// Returns the found value
 		return token, nil
+
 	} else {
+
+		// Returns an error if no value was found
 		return nil, fmt.Errorf("%s doesn't exist in context values", key)
 	}
 }
 
 // Run is the execution of your program with all combined commands.
 func (r *Root) Run() error {
+
+	// Validates the Root object to make sure it's valid.
 	if err := r.validate(); err != nil {
 		return err
 	}
-
+	
+	// Gathers args from an arg wrapper in utils.
 	args := *utils.GetArgs()
-
+	
+	// Creates the context object to store user data.
 	ctx := Context{
 		Values: make(map[string]any),
 		Args: args,
 		ParsedValue: "",
 	}
 	
+	// Checks if the app execution is valid, if there is zero arguments, it returns an error.
 	if len(args) == 0 {
 		return ErrMissingArguments
 	}
-
+	
+	// Loops over every arg given
 	for i, arg := range args {
-		cmd, err := r.parseCommand(&ctx, arg)
 
+		// Checks if the argument is a command, returns nil if not.
+		cmd, err := r.parseCommand(&ctx, arg)
+		
+		// If there is no command, it skips the argument.
 		if cmd == nil {
 			continue
 		}
-
+		
+		// If there is a command parsing error, it'll check and return.
 		if err != nil {
 			return err
 		}
-
+		
+		// Checks if the context is validate.
 		if err := ctx.Validate(); err != nil {
 			return err
 		}
-
+		
+		// If there is 2 or more arguments, it will parse flags.
 		if len(args) >= 2 {
 			if err := cmd.RunFlags(&ctx, args[i+1:]); err != nil {
 				return err
 			}
 		}
 
-
+		// If a command takes a value, it will grab the subsiquent argument and add it to ctx.ParsedValue. As well as add additional arguments to context.
 		if cmd.TakesValue {
 			if i+1 >= len(args) {
 				return fmt.Errorf("command %v requires a value", cmd.Name)
@@ -108,12 +150,15 @@ func (r *Root) Run() error {
 
 			i++
 		}
-
+		
+		// Executes a command using the given context to manipulate.
 		if err := cmd.Execute(&ctx); err != nil {
 			return err
 		} else {
 			return nil
 		}
 	}
+
+	// Returns an error if no commands are found with a given argument.
 	return fmt.Errorf("%w: %s", ErrUnknownCommand, args[0])
 }
