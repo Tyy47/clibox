edeacbbee4b6d39bc10de1f408473487c962b84a
Tyy47 <Tylerwmack321@gmail.com>
2026-09-17 08:54:50 -0400

    wip: created a color interface in colorbin to interact with colors without being able to modify the colors and or input random values

--- colorbin/colorbin.go
+++ colorbin/colorbin.go
@@ -51,8 +51,8 @@ var ansiModifierCodes = map[string]string{
 //   - Background: Stores the background state.
 type Color struct {
 	Value                   any        // Stores the non-colored string to be called upon later
-	ChosenColor             validColor // Stores the called color. Color options are: black, red, green, yellow, blue, magenta, cyan, and white.
-	BackgroundColor         validColor // Stores the called background color. Color options are: black, red, green, yellow, blue, magenta, cyan, and white. Disclaimer: Background color will only be applied if Background is set to true.*
+	ChosenColor             ValidColor // Stores the called color. Color options are: black, red, green, yellow, blue, magenta, cyan, and white.
+	BackgroundColor         ValidColor // Stores the called background color. Color options are: black, red, green, yellow, blue, magenta, cyan, and white. Disclaimer: Background color will only be applied if Background is set to true.*
 	Bold                    bool       // Stores the state for bold. Toggling to true will turn the text bold.
 	Underline               bool       // Stores the state for underline. Toggling to true will make the text underlined.
 	Strikethrough           bool       // Stores the state for strikethrough. Toggling to true will make the text have a strikethrough.
@@ -64,6 +64,12 @@ type Color struct {
 
 // validColor is a private type to restrict colors only supported by this package
 type validColor string
+
+func (v validColor) isValidColor() {}
+
+type ValidColor interface {
+	isValidColor()
+}
 
 // Color constants to identify valid colors for this package
 const (
