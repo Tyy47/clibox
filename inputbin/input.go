d4838242734d2dd67d410a4f8854fbe5f0ecc8bf
Tyy47 <Tylerwmack321@gmail.com>
2026-09-17 07:35:10 -0400

    wip: created prompt function that prompts the user with a question and a selection of answers via a string array. also added an optional prefix to the question as well

--- inputbin/input.go
+++ inputbin/input.go
@@ -1,5 +1,35 @@
 package inputbin
 
+import (
+	"errors"
+	"fmt"
+)
 
+var (
 
+	ErrNoInputGiven  = errors.New("missing required input")
+)
+
+//
+func Prompt(promtPrefix string, question string, answers []string) (string, error) {
+	if promtPrefix == "" {
+		fmt.Println(question)
+	} else {
+		fmt.Printf("%s: %s\n", promtPrefix, question)
+	}
+
+	
+	for i := range answers {
+		fmt.Printf("%d. %s\n", i, answers[i])
+	}
+
+	var userSelection string
+
+	_, err := fmt.Scanln(&userSelection)
+	if err != nil {
+		return "", ErrNoInputGiven
+	}
+
+	return userSelection, nil
+}
 
